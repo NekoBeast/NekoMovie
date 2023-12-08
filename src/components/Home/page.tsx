62f099b49cbbda57173da2c2c1a831521408b532
sushilraii <snarutirai@gmail.com>
2023-12-08 22:09:59 +0545

    Watch or Download movies

---
+++ src/components/Home/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../Style_Components/components.style";
+
+export default function Home(){
+    return(
+        <>
+      <Header>
+      <h1>hello Home</h1>
+      </Header>
+        </>
+    )
+}
---
+++ src/components/Style_Components/components.style.js
@@ -0,0 +1,6 @@
+import style from "style-component";
+
+export const Header = style.dev`
+background-color:red;
+color:green ;
+`;
